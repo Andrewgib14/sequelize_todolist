8a5bc0a892dd0a1a95f03471b21af77b62249ba0
Andrew Gibson <andrewgib14@gmail.com>
2017-08-31 13:30:45 -0500

    adding deleting todo functionality

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const mustacheExpress = require("mustache-express");
 const expressValidator = require("express-validator");
 const path = require("path");
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8004;
 const models = require("./models");
 const app = express();
 const todo = models.todo;
@@ -29,9 +29,9 @@ app.post("/toDoList", function (req, res) {
         is_complete: 'f'
     }).save().then(function (newTodo) {
         console.log(newTodo.item);
+        return res.redirect("/");
     })
 
-    return res.redirect("/");
 })
 
 app.post("/completed", function (req, res) {
@@ -41,8 +41,30 @@ app.post("/completed", function (req, res) {
             where: {
                 item: req.body.item
             }
-        }).then(function (todos) { });
-    return res.redirect("/");
+        }).then(function () {
+            return res.redirect("/");
+        });
+
+})
+
+app.post("/delete", function (req, res) {
+    todo.destroy({
+        where: {
+            is_complete: 't'
+        }
+    }).then(function () {
+        return res.redirect("/");
+    })
+})
+
+app.post("/deleteOne", function (req, res) {
+    todo.destroy({
+        where: {
+            item: req.body.item
+        }
+    }).then(function () {
+        return res.redirect("/")
+    })
 })
 
 app.listen(port, function () {
@@ -50,4 +72,4 @@ app.listen(port, function () {
 })
 
 
-// delete complete todo from the todolist after it is added to the is completed column
+// Edit and Delete Idividual todos
